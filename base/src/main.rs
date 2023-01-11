edb01eadeda2f07bab82b4ebfacac1085b5fa5fc
Yourein <rein.you.3.1@gmail.com>
2023-01-11 20:27:15 +0900

    Implement: environmental variable

--- base/src/main.rs
+++ base/src/main.rs
@@ -3,5 +3,16 @@ use dotenv;
 fn main() {
     dotenv::dotenv().ok();
 
-    println!{"{}", dotenv::var("TEST_VAR").unwrap()};
+    let spotify_key = dotenv::var("SPOTIFY_API_KEY").unwrap();
+    let spotify_secret = dotenv::var("SPOTIFY_API_SECRET").unwrap();
+    let twitter_key = dotenv::var("TWITTER_API_KEY").unwrap();
+    let twitter_secret = dotenv::var("TWITTER_API_SECRET").unwrap();
+
+    println!{
+        "Spotify key: {}\nSpotify secret: {}\nTwitter key: {}\nTwitter secret: {}",
+        spotify_key,
+        spotify_secret,
+        twitter_key,
+        twitter_secret
+    };
 }
