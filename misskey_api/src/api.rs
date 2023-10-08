fa0222898bbed88ba67aac1cd2ffa650fdc596bf
Yourein <rein.you.3.1@gmail.com>
2023-10-08 20:24:11 +0900

    Define MisskeyApi and its function (not implemented, just defined).

---
+++ misskey_api/src/api.rs
@@ -0,0 +1,33 @@
+use interface::PostAPI;
+
+#[allow(dead_code)]
+pub struct MisskeyApi {
+    access_code: String
+}
+
+impl MisskeyApi {
+    pub fn new(code: String) -> Self {
+        MisskeyApi {
+            access_code: code.clone()
+        }
+    }
+
+    pub async fn check_picture_exist(picture: &Bytes) -> Result<bool, String> {
+        TODO!()
+    }
+}
+
+#[async_trait]
+impl PostAPI for MisskeyApi {
+    async fn compose_without_picture(&self, text: &str) -> Result<(), String> {
+        TODO!()
+    }
+
+    async fn compose_with_picture(&self, text: &str, media: &Vec<String>) -> Result<(), String> {
+        TODO!()
+    }
+
+    async fn upload_media(&self, picture: Bytes) -> Option<String> {
+        TODO!()
+    }
+}
