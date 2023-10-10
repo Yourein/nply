3154b950a82d7410f5e35c5a252b59c4231fda24
Yourein <rein.you.3.1@gmail.com>
2023-10-10 11:58:59 +0900

    Implement compose_without_picture

--- misskey_api/src/api.rs
+++ misskey_api/src/api.rs
@@ -1,6 +1,9 @@
+use crate::types::*;
+use crate::types::Responses::*;
 use interface::PostAPI;
 use async_trait::async_trait;
 use bytes::Bytes;
+use reqwest;
 
 #[allow(dead_code)]
 pub struct MisskeyApi {
@@ -28,7 +31,30 @@ impl MisskeyApi {
 #[async_trait]
 impl PostAPI for MisskeyApi {
     async fn compose_without_picture(&self, text: &str) -> Result<(), String> {
-        todo!()
+        let payload = NoteWithoutPicture {
+            i: &self.access_code,
+            text: text.to_string()
+        };
+
+        let res = reqwest::Client::new().post(format!{"{}/notes/create", &self.base_url})
+            .header("Content-Type", "application/json")
+            .json(&payload)
+            .send().await;
+
+        match res {
+            Ok(r) => {
+                if r.status().as_u16() == 200 {
+                    Ok(())
+                }
+                else {
+                    let e = r.json::<CommonError>().await.unwrap();
+                    Err(e.message)
+                }
+            },
+            Err(e) => {
+                Err("Unknown Error Occured".to_string())
+            }
+        }
     }
 
     async fn compose_with_picture(&self, text: &str, media: &Vec<String>) -> Result<(), String> {
