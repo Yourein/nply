b34c2520fd47d8927f4698e75eea7981db6da502
Yourein <rein.you.3.1@gmail.com>
2023-10-11 21:27:38 +0900

    Implement get_image_mime_type()

--- misskey_api/src/api.rs
+++ misskey_api/src/api.rs
@@ -25,8 +25,18 @@ impl MisskeyApi {
         todo!()
     }
 
-    fn hash_picture(picture: &Bytes) -> String {
-        todo!()
+    /// Classify an image to image/jpeg or image/png
+    /// DO NOT input an image that is neither jpeg nor png
+    /// 
+    /// This function is reading 2 bytes from start of the image.
+    /// For detail, please read https://qiita.com/kouheiszk/items/17485ccb902e8190923b#png%E3%81%A7%E3%81%82%E3%82%8B%E3%81%93%E3%81%A8
+    pub(crate) fn get_image_mime_type(&self, picture: &Bytes) -> &'static str {
+        if picture[0..2] == [255, 216] {
+            "image/jpeg"
+        }
+        else {
+            "image/png"
+        }
     }
 
     pub(crate) fn get_endpoint_url(&self, endpoint: &str) -> String {
