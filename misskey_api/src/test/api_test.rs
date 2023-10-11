5650e3c50c66d7ea74c1621bf4d91f87b660485e
Yourein <rein.you.3.1@gmail.com>
2023-10-11 21:37:29 +0900

    Add tests for get_image_mime_type

--- misskey_api/src/test/api_test.rs
+++ misskey_api/src/test/api_test.rs
@@ -4,6 +4,9 @@ mod tests {
     use crate::types::{*, Responses::*};
     use crate::consts::ENDPOINT;
 
+    use bytes::Bytes;
+    use std::fs;
+
     #[test]
     fn endpoint_url_formatting01() {
         let mapi = MisskeyApi::new("dummycred".to_string(), "post.yourein.net".to_string());
@@ -18,5 +21,25 @@ mod tests {
         let res = mapi.get_endpoint_url(ENDPOINT::drive::files::create);
         
         assert_eq!(res, "https://test.hoge.jp/api/drive/files/create".to_string());
+    }
+
+    #[test]
+    fn classify_img_jpeg() {
+        let mapi = MisskeyApi::new("dummycred".to_string(), "test.hoge.jp".to_string());
+        
+        let file = fs::read("./src/test/assets/yourein_test.jpeg").unwrap();
+        let pic: Bytes = Bytes::from(file);
+        let res = mapi.get_image_mime_type(&pic);
+        assert_eq!(res, "image/jpeg");
+    }
+
+    #[test]
+    fn classify_img_png() {
+        let mapi = MisskeyApi::new("dummycred".to_string(), "test.hoge.jp".to_string());
+        
+        let file = fs::read("./src/test/assets/yourein_test.png").unwrap();
+        let pic: Bytes = Bytes::from(file);
+        let res = mapi.get_image_mime_type(&pic);
+        assert_eq!(res, "image/png");
     }
 }
