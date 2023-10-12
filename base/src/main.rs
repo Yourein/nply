fdb6a421afea12ce90e98a73feaee7efebe77435
Yourein <rein.you.3.1@gmail.com>
2023-10-12 10:18:37 +0900

    Add misskey_token and misskey_host to AuthKey (credential)

--- base/src/main.rs
+++ base/src/main.rs
@@ -4,28 +4,35 @@ use dotenv;
 use main_logics::post_current_song;
 use twitter_api::api_model::api::Api as TwitterAPI;
 use spotify_api::api_model::api::Api as SpotifyAPI;
+use misskey_api::api::MisskeyApi;
 use std::io::stdin;
 use std::io::{stdout, Write};
 
 struct AuthKey {
     spotify_key: String,
     spotify_secret: String,
-    twitter_key: String,
-    twitter_secret: String 
+    twitter_key: Result<String, dotenv::Error>,
+    twitter_secret: Result<String, dotenv::Error>,
+    misskey_token: Result<String, dotenv::Error>,
+    misskey_host: Result<String, dotenv::Error>
 }
 
 fn get_credentials() -> AuthKey {
     dotenv::dotenv().ok();
     let spotify_key = dotenv::var("SPOTIFY_API_KEY").unwrap();
     let spotify_secret = dotenv::var("SPOTIFY_API_SECRET").unwrap();
-    let twitter_key = dotenv::var("TWITTER_API_KEY").unwrap();
-    let twitter_secret = dotenv::var("TWITTER_API_SECRET").unwrap();
+    let twitter_key = dotenv::var("TWITTER_API_KEY");
+    let twitter_secret = dotenv::var("TWITTER_API_SECRET");
+    let misskey_token = dotenv::var("MISSKEY_API_TOKEN");
+    let misskey_host = dotenv::var("MISSKEY_HOST");
 
     AuthKey {
         spotify_key: spotify_key,
         spotify_secret: spotify_secret,
         twitter_key: twitter_key,
-        twitter_secret: twitter_secret
+        twitter_secret: twitter_secret,
+        misskey_token: misskey_token,
+        misskey_host: misskey_host
     }
 }
 
